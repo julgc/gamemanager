057f3b0214a2cc4ea554ed8e1ec3484a0cf40cd1
18003-GCHJ <18003-GCHJ@NSNB-065.NOVASOLUTIONSPH>
2018-10-24 17:25:48 +0800

    custom login page

--- gamemanager/src/main/java/com/gm/WebSecurityConfig.java
+++ gamemanager/src/main/java/com/gm/WebSecurityConfig.java
@@ -3,13 +3,14 @@ package com.gm;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -30,11 +31,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		//@formatter:off
-		http.authorizeRequests().antMatchers("/").permitAll()
+		http.authorizeRequests().anyRequest().authenticated()
+        .and().authorizeRequests().antMatchers("/").permitAll()
 		.and().authorizeRequests().antMatchers("/user","/user/**").access("hasRole('ROLE_USER')")
 		.and().authorizeRequests().antMatchers("/admin","/admin/**").access("hasRole('ROLE_ADMIN')")
-		.and().formLogin().usernameParameter("username").passwordParameter("password").and().csrf();
-		//.and().exceptionHandling().accessDeniedPage("/Access_Denied");
+		.and().formLogin().loginPage("/login").failureHandler(customAuthenticationFailureHandler()).permitAll()
+		.usernameParameter("username").passwordParameter("password")
+		.and().logout().permitAll()
+		.and().csrf();
 		//@formatter:on
+	}
+
+	@Bean
+	public AuthenticationFailureHandler customAuthenticationFailureHandler() {
+		return new CustomAuthenticationFailureHandler();
 	}
 }
--- gamemanager/src/main/java/com/gm/controller/MainController.java
+++ gamemanager/src/main/java/com/gm/controller/MainController.java
@@ -2,11 +2,17 @@ package com.gm.controller;
 
 import java.util.Collection;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -22,13 +28,34 @@ public class MainController {
 		return "USER ACCESS";
 	}
 
+	@GetMapping("/test")
+	public String test() {
+		return "/views/test";
+	}
+
+	@GetMapping("/login")
+	public String login(@RequestParam(required = false) String error, @RequestParam(required = false) String logout,
+			ModelMap map, HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (logout != null) {
+			if (auth != null) {
+				new SecurityContextLogoutHandler().logout(request, response, auth);
+			}
+			map.put("msg", "You've been logged out in the system...");
+		}
+		if (error != null) {
+			map.put("msg", "Unable to login please check username and password");
+		}
+		return "/views/login";
+	}
+
 	@GetMapping("/")
 	public String home() {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		Collection<? extends GrantedAuthority> userRoles = auth.getAuthorities();
-		
+
 		String returnPath = "redirect:/login";
-		
+
 		for (GrantedAuthority role : userRoles) {
 			if (role.getAuthority().equals("ROLE_USER")) {
 				returnPath = "redirect:/user";
